9f8cbe98f4a5301d4db7ce2605c4c8b1197fe2eb
Daeram Chung <daeram.chung@gmail.com>
2023-10-16 16:09:48 +0900

    chore: React query 설치 및 설정

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -9,8 +10,12 @@ if (process.env.NODE_ENV === "development") {
   });
 }
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
