d2871eef1bd1f6dc63f1add193712953386da226
Daeram Chung <daeram.chung@gmail.com>
2023-10-16 16:47:42 +0900

    feat: Router 설정

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,3 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "router/router";
+
 export default function App() {
-  return <>FineAnts!</>;
+  const user = {}; // TODO
+
+  return <RouterProvider router={router(user)} />;
 }
