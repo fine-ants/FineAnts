dfc3be15c98a9d6007b9d177831eb57c000353f0
YongHwan Kim <yonghwankim.dev@gmail.com>
2023-10-17 22:37:55 +0900

    [feat] 소셜 로그인 및 로그아웃 서비스 구현 (#14)
    * #6 chore: jwt 관련 라이브러리 추가
    
    * #6 docs: gitignore에 푸시하지 말아야할 파일들 추가
    
    * #6 docs: 로컬 환경 docker-compose.yml 추가
    
    * #2 docs: applicatoin.yml 파일 설정 및 로그백 파일 추가
    
    * #6 feat: 회원 엔티티 구현
    
    * #6 feat: 에러관련 코드, 예외, 핸들러 추가
    
    * #6 feat: 소셜 로그인 서비스 구현
    
    * #6 rename: oauth 파일 이동
    
    * #6 feat: Oauth 로그인 서비스 구현
    
    * #6 feat: 로그아웃 및 액세스 토큰 갱신 구현

---
+++ fineAnts_app/src/main/java/codesquad/fineants/spring/config/OauthConfig.java
@@ -0,0 +1,22 @@
+package codesquad.fineants.spring.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import codesquad.fineants.domain.oauth.properties.OauthProperties;
+import codesquad.fineants.domain.oauth.repository.InMemoryOauthClientRepository;
+import lombok.RequiredArgsConstructor;
+
+@EnableConfigurationProperties(OauthProperties.class)
+@RequiredArgsConstructor
+@Configuration
+public class OauthConfig {
+
+	private final OauthProperties oauthProperties;
+
+	@Bean
+	public InMemoryOauthClientRepository inMemoryOauthProviderRepository() {
+		return new InMemoryOauthClientRepository(oauthProperties.createOauthClientMap());
+	}
+}
