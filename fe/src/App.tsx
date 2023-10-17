1cb12432687d3101c8c5a784d47594d16ef0fe06
Daeram Chung <daeram.chung@gmail.com>
2023-10-17 16:48:21 +0900

    #4 design: GlobalStyles 및 기본 css 적용

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,8 +1,16 @@
+import useUserInfoQuery from "@api/auth/queries/useUserInfoQuery";
 import { RouterProvider } from "react-router-dom";
 import router from "router/router";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "styles/GlobalStyles";
 
 export default function App() {
-  const user = {}; // TODO
+  const { data: user } = useUserInfoQuery();
 
-  return <RouterProvider router={router(user)} />;
+  return (
+    <ThemeProvider theme={{}}>
+      <GlobalStyles />
+      <RouterProvider router={router(user)} />
+    </ThemeProvider>
+  );
 }
--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./index.css";
 import browserServiceWorker from "./mocks/browserServiceWorker.ts";
 
 if (process.env.NODE_ENV === "development") {
