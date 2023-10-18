bcfa017a4a6f12855106509f14b7944ed23c7b90
Jay <chlwodnjs34@gmail.com>
2023-10-18 17:47:56 +0900

    #11 feat : MyProfilePage 구현
    - App.tsx에 path 문제 수정
    - PortfoliosPage 구현
    - ProfileEditPage 구현

---
+++ fe/src/pages/MyProfilePage.tsx
@@ -0,0 +1,36 @@
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Routes from "../router/Routes";
+import PortfoliosPage from "./PortfoliosPage";
+import ProfileEditPage from "./ProfileEditPage";
+
+export default function MyProfilePage() {
+  const navigate = useNavigate();
+  const { section } = useParams();
+  const [selectedSection, setSelectedSection] = useState(section || "edit");
+
+  const onChange = (_: React.MouseEvent<HTMLElement>, section: string) => {
+    if (!section) return;
+
+    setSelectedSection(section);
+    navigate(`${Routes.PROFILE}/${section}`);
+  };
+
+  return (
+    <>
+      <ToggleButtonGroup
+        color="primary"
+        value={selectedSection}
+        exclusive
+        onChange={onChange}
+        aria-label="Platform">
+        <ToggleButton value="edit">내 프로필</ToggleButton>
+        <ToggleButton value="portfolios">내 포트폴리오</ToggleButton>
+      </ToggleButtonGroup>
+
+      {section === "edit" ? <ProfileEditPage /> : <PortfoliosPage />}
+    </>
+  );
+}
