8ea3caecf1041e351a1c64c5eef8bbad1e968d99
bakhacode <bakhamitai@naver.com>
2023-10-19 17:14:48 +0900

    #30 refactor: currentValue 컴포넌트로 분리

--- fe/src/pages/DashboardPage.tsx
+++ fe/src/pages/DashboardPage.tsx
@@ -1,7 +1,10 @@
+import CurrentValues from "@components/Dashboard/CurrentValues";
 import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
 
 import styled from "styled-components";
+
+import PortfolioPieChart from "../components/Dashboard/PortfolioPieChart";
 
 export default function DashboardPage() {
   return (
@@ -9,31 +12,11 @@ export default function DashboardPage() {
       <Header />
       <Main>
         <MainColorBackground />
-        <CurrentValueContainer>
-          <TotalMainContentWrapper>
-            <MainText>총 평가 금액</MainText>
-            <MainText>2,540,000 ₩</MainText>
-          </TotalMainContentWrapper>
-          <TotalSubContentWrapper>
-            <SubText>총 투자 금액</SubText>
-            <SubText>1,273,000 ₩</SubText>
-            <SubText></SubText>
-          </TotalSubContentWrapper>
-          <TotalSubContentWrapper>
-            <SubText>총 손익 </SubText>
-            <SubText>+ 1,500,000</SubText>
-            <SubText>+ 100%</SubText>
-          </TotalSubContentWrapper>
-          <TotalSubContentWrapper>
-            <SubText>연 배당금</SubText>
-            <SubText>₩ 200,000</SubText>
-            <SubText>+ 10%</SubText>
-          </TotalSubContentWrapper>
-        </CurrentValueContainer>
+        <CurrentValues />
         <CurrentChartContainer>
           <DateRangeSelector>1D 1W 1M 1Q 1Y All</DateRangeSelector>
           <ChartContainer>
-            <PieChart />
+            <PortfolioPieChart />
             <LineChart />
           </ChartContainer>
         </CurrentChartContainer>
@@ -69,48 +52,6 @@ const MainColorBackground = styled.div`
   z-index: 0;
 `;
 
-const CurrentValueContainer = styled.div`
-  width: 1440px;
-  height: 144px;
-  display: flex;
-  gap: 24px;
-  position: absolute;
-  top: 112px;
-  padding: 0 80px;
-  z-index: 3;
-`;
-
-const TotalMainContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  width: 330px;
-  height: 100%;
-`;
-
-const MainText = styled.div`
-  font-size: 54px;
-  font-weight: bold;
-`;
-
-const TotalSubContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  width: 160px;
-  height: 100%;
-`;
-
-const SubText = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 24px;
-  font-weight: bold;
-  height: 100%;
-`;
-
 const CurrentChartContainer = styled.div`
   width: 1202px;
   height: 428px;
@@ -128,6 +69,12 @@ const DateRangeSelector = styled.div`
   width: 256px;
   height: 32px;
   margin-left: auto;
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: #d6d3d3;
 `;
 
@@ -138,12 +85,6 @@ const ChartContainer = styled.div`
   justify-content: center;
   align-items: flex-end;
   gap: 54px;
-`;
-
-const PieChart = styled.div`
-  width: 460px;
-  height: 384px;
-  background-color: #d6d3d3;
 `;
 
 const LineChart = styled.div`
