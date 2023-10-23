47ee0f36afec3edb7ca40acffd350bb489e342e7
Daeram Chung <daeram.chung@gmail.com>
2023-10-23 17:06:36 +0900

    #4 fix: Window.naver doesn't exist type error

--- fe/src/components/auth/NaverSignInButton.tsx
+++ fe/src/components/auth/NaverSignInButton.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import styled from "styled-components";
 
 export default function NaverSignInButton() {
-  const naverLogin = new window.naver.LoginWithNaverId({
+  const naverLogin = new (window as any).naver.LoginWithNaverId({
     clientId: import.meta.env.VITE_NAVER_CLIENT_ID,
     callbackUrl: `${import.meta.env.VITE_CLIENT_URL}/signin?provider=naver`,
     isPopup: true,
@@ -17,11 +17,7 @@ export default function NaverSignInButton() {
     naverLogin.init();
   }, []);
 
-  return (
-    <StyledNaverSignInButton id="naverIdLogin">
-      Naver 로그인
-    </StyledNaverSignInButton>
-  );
+  return <StyledNaverSignInButton id="naverIdLogin" />;
 }
 
 const StyledNaverSignInButton = styled.div``;
