3e95e68fd54bdcb1e984a14e9121d9804544e6a6
Daeram Chung <daeram.chung@gmail.com>
2023-10-25 20:37:26 +0900

    #44 feat: 포트폴리오 상세 조회 및 종목 조회 API 및 mock 추가

--- fe/src/mocks/handlers/portfolioHandlers.ts
+++ fe/src/mocks/handlers/portfolioHandlers.ts
@@ -1,12 +1,24 @@
 import { HTTPSTATUS } from "@api/types";
-import { successfulPortfolioData } from "mocks/data/portfolioData";
+import {
+  successfulPortfolioData,
+  successfulPortfolioDetailsData,
+} from "mocks/data/portfolioData";
 import { rest } from "msw";
 
 export default [
+  // List of portfolios
   rest.get("/api/portfolios", async (_, res, ctx) => {
     return res(
       ctx.status(HTTPSTATUS.success),
       ctx.json(successfulPortfolioData)
+    );
+  }),
+
+  // Portfolio details
+  rest.get("/api/portfolio/:portfolioId/holdings", async (_, res, ctx) => {
+    return res(
+      ctx.status(HTTPSTATUS.success),
+      ctx.json(successfulPortfolioDetailsData)
     );
   }),
 ];
