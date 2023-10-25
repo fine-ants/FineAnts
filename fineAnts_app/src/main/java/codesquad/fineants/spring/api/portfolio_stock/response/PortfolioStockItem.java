9895b8a3f813263878b3737fa8f9aa01e5263d3b
yonghwankim.dev <dragonbead95@naver.com>
2023-10-25 15:47:44 +0900

    #10 feat: @JsonUnWraaped 적용

--- fineAnts_app/src/main/java/codesquad/fineants/spring/api/portfolio_stock/response/PortfolioStockItem.java
+++ fineAnts_app/src/main/java/codesquad/fineants/spring/api/portfolio_stock/response/PortfolioStockItem.java
@@ -3,13 +3,18 @@ package codesquad.fineants.spring.api.portfolio_stock.response;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+
 import codesquad.fineants.domain.portfolio_stock.PortfolioHolding;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class PortfolioStockItem {
+
+	@JsonUnwrapped
 	private StockItem stock;
+	@JsonUnwrapped
 	private PortfolioHoldingDetailItem portfolioHolding;
 	private List<TradeHistoryItem> tradeHistories;
 
