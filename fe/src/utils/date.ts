13cef44074bd6f538294f071a92efa146643ec98
Daeram Chung <daeram.chung@gmail.com>
2023-10-27 02:13:39 +0900

    #44 feat: validateDateFormat util 함수 추가

--- fe/src/utils/date.ts
+++ fe/src/utils/date.ts
@@ -1,3 +1,4 @@
+// Convert YYYY-MM-DDTHH:MM:SS to YYYY-MM-DD
 export function formatDate(dateStr: string): string {
   const dateStrRegex = new RegExp(
     /\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T(0[0-9]|1[0-9]|2[0-4]):([0-5][0-9]):([0-5][0-9])/
@@ -12,4 +13,14 @@ export function formatDate(dateStr: string): string {
   const day = date.getDate();
 
   return `${year}-${month}-${day}`;
+}
+
+// Validate YYYY-MM-DD
+export function validateDateFormat(dateStr: string) {
+  const dateStrRegex = new RegExp(
+    /\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])/
+  );
+  if (!dateStrRegex.test(dateStr)) {
+    throw Error("날짜 형식은 YYYY-MM-DD 입니다");
+  }
 }
