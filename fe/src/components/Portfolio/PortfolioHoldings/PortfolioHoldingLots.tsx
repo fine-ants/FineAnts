44b47bd6688a5a69ab493d91f256783bb361457d
Daeram Chung <daeram.chung@gmail.com>
2023-10-27 18:22:39 +0900

    #44 refactor: Portfolio holding lot 개별 수정 및 저장으로 변경

--- fe/src/components/Portfolio/PortfolioHoldings/PortfolioHoldingLots.tsx
+++ fe/src/components/Portfolio/PortfolioHoldings/PortfolioHoldingLots.tsx
@@ -1,68 +1,35 @@
 import { PurchaseHistoryField } from "@api/portfolio";
 import {
-  Button,
   Table,
   TableBody,
   TableCell,
-  TableFooter,
   TableHead,
   TableRow,
 } from "@mui/material";
-import { useState } from "react";
 import PortfolioHoldingLotRow from "./PortfolioHoldingLotRow";
-import PortfolioHoldingLotRowEdit from "./PortfolioHoldingLotRowEdit";
 
 type Props = {
   purchaseHistory: PurchaseHistoryField[];
 };
 
 export default function PortfolioHoldingLots({ purchaseHistory }: Props) {
-  const [isEditing, setIsEditing] = useState(false);
-
-  const onEditLotsButtonClick = () => {
-    setIsEditing(true);
-  };
-
-  const onLotChangesSave = () => {
-    // TODO: Send request to save changes
-    setIsEditing(false);
-  };
-
   return (
     <>
       <Table size="small" aria-label="purchases">
         <TableHead>
           <TableRow>
             <TableCell>매입 날짜</TableCell>
-            <TableCell>매입가</TableCell>
+            <TableCell align="right">매입가</TableCell>
             <TableCell align="right">매입 개수</TableCell>
             <TableCell align="right">메모</TableCell>
+            <TableCell />
           </TableRow>
         </TableHead>
         <TableBody>
-          {purchaseHistory.map((lot) => {
-            return isEditing ? (
-              <PortfolioHoldingLotRowEdit key={lot.id} {...lot} />
-            ) : (
-              <PortfolioHoldingLotRow key={lot.id} {...lot} />
-            );
-          })}
+          {purchaseHistory.map((lot) => (
+            <PortfolioHoldingLotRow key={lot.id} {...lot} />
+          ))}
         </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell align="right" colSpan={4}>
-              {isEditing ? (
-                <Button variant="text" onClick={onLotChangesSave}>
-                  저장
-                </Button>
-              ) : (
-                <Button variant="text" onClick={onEditLotsButtonClick}>
-                  수정
-                </Button>
-              )}
-            </TableCell>
-          </TableRow>
-        </TableFooter>
       </Table>
     </>
   );
--- fe/src/components/Portfolio/PortfolioHoldings/PortfolioHoldingsTable.tsx
+++ fe/src/components/Portfolio/PortfolioHoldings/PortfolioHoldingsTable.tsx
@@ -20,14 +20,14 @@ export default function PortfolioHoldingsTable({ portfolioId, data }: Props) {
       <TableHead sx={{ border: "none", borderRadius: "8px" }}>
         <ColumnHeader>
           <TableCell />
-          <ColumnHeaderField>종목명</ColumnHeaderField>
-          <ColumnHeaderField align="right">평가금액</ColumnHeaderField>
-          <ColumnHeaderField align="right">현재가</ColumnHeaderField>
-          <ColumnHeaderField align="right">평균 매입가</ColumnHeaderField>
-          <ColumnHeaderField align="right">개수</ColumnHeaderField>
-          <ColumnHeaderField align="right">변동률</ColumnHeaderField>
-          <ColumnHeaderField align="right">총 손익</ColumnHeaderField>
-          <ColumnHeaderField align="right">연 배당금</ColumnHeaderField>
+          <ColumnHeaderCell>종목명</ColumnHeaderCell>
+          <ColumnHeaderCell align="right">평가금액</ColumnHeaderCell>
+          <ColumnHeaderCell align="right">현재가</ColumnHeaderCell>
+          <ColumnHeaderCell align="right">평균 매입가</ColumnHeaderCell>
+          <ColumnHeaderCell align="right">개수</ColumnHeaderCell>
+          <ColumnHeaderCell align="right">변동률</ColumnHeaderCell>
+          <ColumnHeaderCell align="right">총 손익</ColumnHeaderCell>
+          <ColumnHeaderCell align="right">연 배당금</ColumnHeaderCell>
         </ColumnHeader>
       </TableHead>
 
@@ -48,6 +48,6 @@ const ColumnHeader = styled(TableRow)`
   background-color: #f6f6f8;
 `;
 
-const ColumnHeaderField = styled(TableCell)`
+const ColumnHeaderCell = styled(TableCell)`
   padding: 5px 0;
 `;
