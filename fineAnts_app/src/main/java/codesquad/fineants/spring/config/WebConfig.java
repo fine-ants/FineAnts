dade0de1a94ac54191099358d4e7fe6cf1262620
yonghwankim.dev <dragonbead95@naver.com>
2023-10-31 13:51:30 +0900

    #75 fix: cors 설정

--- fineAnts_app/src/main/java/codesquad/fineants/spring/config/WebConfig.java
+++ fineAnts_app/src/main/java/codesquad/fineants/spring/config/WebConfig.java
@@ -3,7 +3,9 @@ package codesquad.fineants.spring.config;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,6 +18,19 @@ import lombok.RequiredArgsConstructor;
 public class WebConfig implements WebMvcConfigurer {
 
 	private final AuthPrincipalArgumentResolver authPrincipalArgumentResolver;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods(HttpMethod.GET.name(),
+				HttpMethod.POST.name(),
+				HttpMethod.PUT.name(),
+				HttpMethod.DELETE.name(),
+				HttpMethod.PATCH.name(),
+				HttpMethod.OPTIONS.name())
+			.maxAge(3600);
+	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
