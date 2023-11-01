3cc23241b52bda7d42a343a94ea072962f5d9393
Jay <chlwodnjs34@gmail.com>
2023-11-01 11:39:38 +0900

    #77 refactor : 잘못 수정한 로직 변경

--- fe/src/api/portfolio/queries/usePortfolioDeleteMutation.ts
+++ fe/src/api/portfolio/queries/usePortfolioDeleteMutation.ts
@@ -1,18 +1,13 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { deletePortfolio } from "..";
 import { portfolioKeys } from "./queryKeys";
 
 export default function usePortfolioDeleteMutation(portfolioId: number) {
-  const queryClient = useQueryClient();
-
   return useMutation({
     mutationKey: portfolioKeys.details(portfolioId).queryKey,
     mutationFn: deletePortfolio,
     onSuccess: () => {
-      // TODO: toast
-      queryClient.invalidateQueries(
-        portfolioKeys.details(portfolioId).queryKey
-      );
+      // TODO: toast, 포트폴리오 목록 query invalidate
     },
   });
 }
--- fe/src/api/portfolio/queries/usePortfolioHoldingDeleteMutation.ts
+++ fe/src/api/portfolio/queries/usePortfolioHoldingDeleteMutation.ts
@@ -1,13 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deletePortfolioHolding } from "..";
 import { portfolioKeys } from "./queryKeys";
 
 export default function usePortfolioHoldingAddMutation(portfolioId: number) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationKey: portfolioKeys.deleteHolding(portfolioId).queryKey,
     mutationFn: deletePortfolioHolding,
     onSuccess: () => {
-      // TODO: toast, 포트폴리오 목록 query invalidate
+      // TODO: toast
+      queryClient.invalidateQueries({
+        queryKey: portfolioKeys.details(portfolioId).queryKey,
+      });
     },
   });
 }
