424df15a3de651f658fcc913de500c3852a6434b
Jay <chlwodnjs34@gmail.com>
2023-11-01 11:24:20 +0900

    #77 refactor : 불필요한 invalidate 제거

--- fe/src/api/portfolio/queries/usePortfolioHoldingDeleteMutation.ts
+++ fe/src/api/portfolio/queries/usePortfolioHoldingDeleteMutation.ts
@@ -1,18 +1,13 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { deletePortfolioHolding } from "..";
 import { portfolioKeys } from "./queryKeys";
 
 export default function usePortfolioHoldingAddMutation(portfolioId: number) {
-  const queryClient = useQueryClient();
-
   return useMutation({
     mutationKey: portfolioKeys.deleteHolding(portfolioId).queryKey,
     mutationFn: deletePortfolioHolding,
     onSuccess: () => {
-      // TODO: toast
-      queryClient.invalidateQueries(
-        portfolioKeys.details(portfolioId).queryKey
-      );
+      // TODO: toast, 포트폴리오 목록 query invalidate
     },
   });
 }
