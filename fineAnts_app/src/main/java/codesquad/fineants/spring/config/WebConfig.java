f6830f407c3c65dc31365b0cb8cf6740a9062ba2
YongHwan Kim <yonghwankim.dev@gmail.com>
2023-11-02 13:01:06 +0900

    [fix] corsFilter 추가 (#118)

--- fineAnts_app/src/main/java/codesquad/fineants/spring/config/WebConfig.java
+++ fineAnts_app/src/main/java/codesquad/fineants/spring/config/WebConfig.java
@@ -3,9 +3,7 @@ package codesquad.fineants.spring.config;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,21 +16,6 @@ import lombok.RequiredArgsConstructor;
 public class WebConfig implements WebMvcConfigurer {
 
 	private final AuthPrincipalArgumentResolver authPrincipalArgumentResolver;
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOrigins("*")
-			.allowedHeaders("*")
-			.allowedMethods(HttpMethod.GET.name(),
-				HttpMethod.POST.name(),
-				HttpMethod.PUT.name(),
-				HttpMethod.DELETE.name(),
-				HttpMethod.PATCH.name(),
-				HttpMethod.OPTIONS.name(),
-				HttpMethod.HEAD.name())
-			.maxAge(3600);
-	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
