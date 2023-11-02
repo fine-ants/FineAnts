3108e9b6052cab4455d88ae2cdea810d2267181a
yonghwankim.dev <dragonbead95@naver.com>
2023-11-02 12:13:39 +0900

    #22 fix: cors 설정

--- fineAnts_app/src/main/java/codesquad/fineants/spring/config/WebConfig.java
+++ fineAnts_app/src/main/java/codesquad/fineants/spring/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,6 +15,7 @@ import codesquad.fineants.spring.intercetpor.LogoutInterceptor;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -23,12 +25,14 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("*")
+			.allowedHeaders("*")
 			.allowedMethods(HttpMethod.GET.name(),
 				HttpMethod.POST.name(),
 				HttpMethod.PUT.name(),
 				HttpMethod.DELETE.name(),
 				HttpMethod.PATCH.name(),
-				HttpMethod.OPTIONS.name())
+				HttpMethod.OPTIONS.name(),
+				HttpMethod.HEAD.name())
 			.maxAge(3600);
 	}
 
