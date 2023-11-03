d68d130b6894f027883f14c41588d331c4cc01c0
bakhacode <bakhamitai@naver.com>
2023-11-03 13:01:38 +0900

    style: 주석 제거 및 타입 속성 이름 변경

--- fe/src/pages/ProfilePage/PortfoliosPage.tsx
+++ fe/src/pages/ProfilePage/PortfoliosPage.tsx
@@ -4,48 +4,6 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { thousandsDelimiter } from "../../utils/thousandsDelimiter";
 
-// type Portfolio = {
-//   id: number;
-//   securitiesFirmImage: string;
-//   name: string;
-//   budget: number;
-//   totalGain: number;
-//   totalReturnRate: number;
-//   dailyGain: number;
-//   dailyReturnRate: number;
-//   expectedMonthlyDividend: number;
-//   numShares: number;
-// };
-
-// const SAMPLE_PORTFOLIO = [
-//   {
-//     id: 0,
-//     securitiesFirmImage:
-//       "https://framerusercontent.com/images/y7135TGP0TiQ7gtLbQ0IrWOzww.jpg",
-//     name: "물린게아니라장기투자",
-//     budget: 1000000,
-//     totalGain: 10,
-//     totalReturnRate: 100000,
-//     dailyGain: 10,
-//     dailyReturnRate: 100000,
-//     expectedMonthlyDividend: 20000,
-//     numShares: 12,
-//   },
-//   {
-//     id: 1,
-//     securitiesFirmImage:
-//       "https://framerusercontent.com/images/y7135TGP0TiQ7gtLbQ0IrWOzww.jpg",
-//     name: "롱숏롱숏",
-//     budget: 1000000,
-//     totalGain: -5,
-//     totalReturnRate: 50000,
-//     dailyGain: -5,
-//     dailyReturnRate: 50000,
-//     expectedMonthlyDividend: 1000,
-//     numShares: 1,
-//   },
-// ];
-
 export default function PortfoliosPage() {
   const { data: portfolioList } = usePortfolioListQuery();
 
